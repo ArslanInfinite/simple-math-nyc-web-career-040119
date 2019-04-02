0ef656a79d33576e57e1a189a10f16f582d10902
Arslan Ali <ali.arslan319@gmail.com>
2019-04-02 21:35:38 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,3 +1,13 @@
-def addition
-  5 + 4
+def addition(5, 4)
+  
 end
+
+
+
+# require 'spec_helper'
+# 
+# describe "#addition" do 
+#   it "adds two numbers together" do
+#     expect(addition(5,4)).to eq(9)
+#   end
+# end
